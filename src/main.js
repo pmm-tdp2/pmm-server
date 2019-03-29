9d9876a8adf19404dafd5c43900787604a0299a1
harroyo <harroyo@sysone.com>
2019-03-29 14:44:48 -0300

    TASK 97, TASK 98, TASK 99, TASK 101

--- src/main.js
+++ src/main.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const app = express();
+const PORT = 3000;
+require ('custom-env').env('pmm')
 
 app.get("/home", (req,res) => {
-    console.log("sending books");
+    console.log("response " + req.url);
     res.send("Hello World !!!")
 });
 
-app.listen(process.env.PORT || 3000, ()=> {
-    console.log("Listening to the port", app.PORT);
+app.listen(process.env.PORT || PORT, ()=> {
+    console.log("Listening to the port : " + process.env.PORT);
 })
 
 process.on('uncaughtException', (err) => {
